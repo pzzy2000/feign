38cf155cffd52205b85af66ac3c753fd3ee1ffc1
zy <zy@zy.com>
2021-11-12 19:35:59 +0800

    还原

--- core/src/main/java/feign/ReflectiveFeign.java
+++ core/src/main/java/feign/ReflectiveFeign.java
@@ -219,20 +219,14 @@ public class ReflectiveFeign extends Feign {
       for (Entry<Integer, Collection<String>> entry : metadata.indexToName().entrySet()) {
         int i = entry.getKey();
         Object value = argv[entry.getKey()];
-        if(metadata.isFormModule()) {
+        if (value != null) { // Null values are skipped.
+          if (indexToExpander.containsKey(i)) {
+            value = expandElements(indexToExpander.get(i), value);
+          }
           for (String name : entry.getValue()) {
             varBuilder.put(name, value);
           }
-        }else {
-          if (value != null) { // Null values are skipped.
-            if (indexToExpander.containsKey(i)) {
-              value = expandElements(indexToExpander.get(i), value);
-            }
-            for (String name : entry.getValue()) {
-              varBuilder.put(name, value);
-            }
-          }   
-        }
+        }  
       }
 
       RequestTemplate template = resolve(argv, mutable, varBuilder);
